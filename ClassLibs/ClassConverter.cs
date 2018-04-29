9ef560644689d73567eb337288cdf190b2149919
Yusuf Ahmadi <yusufahmadi@users.noreply.github.com>
2018-04-29 21:16:51 +0700

    initial

---
+++ ClassLibs/ClassConverter.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YLibs
+    {
+    public class ClassConverter
+    {
+        public static bool IsDate(Object obj)
+        {
+            string strDate = obj.ToString();
+            try
+            {
+                DateTime dt = DateTime.Parse(strDate);
+                if ((dt.Month != System.DateTime.Now.Month) || (dt.Day < 1 && dt.Day > 31) || dt.Year != System.DateTime.Now.Year)
+                    return false;
+                else
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static DateTime NulltoDate(object x)
+        {
+            DateTime d = Convert.ToDateTime("1900-01-01");
+            if (IsDate(x))
+            {
+                d = Convert.ToDateTime(x);
+            }
+            return d;
+        }
+        public static int NullToInt32(Object val)
+        {
+            int r = -1;
+            try
+            {
+                r= Convert.ToInt32(val);
+            }
+            catch (Exception)
+            {            }
+            return r;
+        }
+        public static long NullToLong(Object val)
+        {
+            long r = -1;
+            try
+            {
+                r = Convert.ToInt64(val);
+            }
+            catch (Exception)
+            { }
+            return r;
+        }
+        public static double NullToDbl(Object val)
+        {
+            double r = 0.0;
+            try
+            {
+                r = Convert.ToDouble(val);
+            }
+            catch (Exception)
+            { }
+            return r;
+        }
+        public static Boolean NullToBool(Object val)
+        {
+            bool r = false;
+            try
+            {
+                r = Convert.ToBoolean(val);
+            }
+            catch (Exception)
+            { }
+            return r;
+        }
+
+        public string FixApostropi(object obj)
+        {
+            string x = "";
+            try
+            {
+                x = obj.ToString().Replace("'", "''");
+            }
+            catch (Exception )
+            {
+                x = "";
+            }
+
+            return x;
+        }
+        
+        //string x1 = 1 == 1 ? "true" : "false";
+
+        public string FixKoma(object obj)
+        {            
+            string x = "";
+            try
+            {
+                x = obj.ToString().Replace(",", ".");
+            }
+            catch (Exception )
+            {
+                x = "";
+            }
+
+            return x;
+        }
+
+    }
+}
---
+++ ClassLibs/ClassSQLServer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YLibs
+{
+    class ClassSQLServer
+    {
+        public static string SqlConn = "";
+        public static string SqlConn2 = "";
+        public DataSet ExecuteDataset(string tabel, string SQL,bool GunakanServer2= false) { 
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection(GunakanServer2 ? SqlConn2 : SqlConn)) {
+                using (SqlCommand cmd = new SqlCommand(SQL, con)) {
+                    using (SqlDataAdapter ODA = new SqlDataAdapter(cmd)) {
+                        try {
+                            con.Open();
+                            cmd.CommandTimeout = con.ConnectionTimeout;
+                            if (ds.Tables[tabel] != null){
+                                ds.Tables[tabel].Clear();
+                            }
+                            ODA.Fill(ds, tabel);
+                        }
+                        catch (Exception){
+                            ds = null;
+                        }
+                    }
+                }
+            }
+            return ds;
+        }
+
+        public int EksekusiSQL(string SQl, string KoneksiString = "", bool IsTampilError = true)
+        {
+            SqlConnection oConn = null /* TODO Change to default(_) if this is not a reference type */;
+            SqlCommand ocmd = null /* TODO Change to default(_) if this is not a reference type */;
+            int NoID=0;
+            //string KoneksiStringAktif;
+            //if (KoneksiString == "")
+            //    KoneksiStringAktif = SqlConn;
+            //else
+            //    KoneksiStringAktif = SqlConn;
+            try
+            {
+                oConn = new SqlConnection(SqlConn);
+                ocmd = new SqlCommand(SQl, oConn);
+                oConn.Open();
+                ocmd.CommandTimeout = oConn.ConnectionTimeout;
+                NoID = ClassConverter.NullToInt32(ocmd.ExecuteNonQuery());
+             }
+            catch (Exception)
+            {
+                //if (IsTampilError)
+                //    XtraMessageBox.Show("Gagal mengeksekusi SQL :" + SQl + vbCrLf + ex.Message, NamaAplikasi, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                if (ocmd != null)
+                    ocmd.Dispose();
+                if (oConn != null)
+                {
+                    oConn.Close();
+                    oConn.Dispose();
+                }
+            }
+            return NoID;
+        }
+
+        public object EksekusiSQlSkalarNew(string strsql)
+        {
+            SqlConnection oConn = null /* TODO Change to default(_) if this is not a reference type */;
+            SqlCommand ocmd = null /* TODO Change to default(_) if this is not a reference type */;
+            object hasil = "";
+            try
+            {
+                oConn = new SqlConnection(SqlConn);
+                ocmd = new SqlCommand(strsql, oConn);
+                oConn.Open();
+                ocmd.CommandTimeout = oConn.ConnectionTimeout;
+                hasil = ocmd.ExecuteScalar();
+            }
+            catch (Exception )
+            {
+            }
+            finally
+            {
+                if (ocmd != null)
+                    ocmd.Dispose();
+                if (oConn != null)
+                {
+                    oConn.Close();
+                    oConn.Dispose();
+                }
+            }
+            return hasil;
+        }
+
+    }
+}
